4c985bdb90376ac0c8520671deb2d26c7b128a43
9jwollan <31480922+9jwollan@users.noreply.github.com>
2017-10-20 09:16:30 -0500

    Add files via upload

---
+++ my object - Copy.js
@@ -0,0 +1,13 @@
+function pets(n,s){
+      this.name=n
+      this.size=s
+this.petclassification=function(typesofpets){
+console.log ("your pet is a +n and it is +s")
+
+};
+
+}
+
+let pet=new pets('lizard',"small");
+console.log(pet.name);
+console.log(pet.size)
---
+++ teams.js
@@ -0,0 +1,15 @@
+function teams(n,w,l){
+      this.name=n
+      this.win=w
+      this.loss=l
+this.record=function(record){
+console.log ("your team is +w and +l")
+
+};
+
+}
+
+let team=new teams('ravens',"three","three");
+console.log(team.name);
+console.log(team.win)
+console.log(team.loss);
